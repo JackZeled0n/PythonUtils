1040659b613473f7ea0aec113441614f2e908b5f
Jake Antonio Ruiz Zeledón <jackiezeledon@gmail.com>
2018-09-03 11:50:04 -0600

    Update VariablesSqltoJavaCamelCase.py
    Agregar tipo de datos sql para convertirlos en tipo de datos java

--- VariablesSqltoJavaCamelCase.py
+++ VariablesSqltoJavaCamelCase.py
@@ -11,4 +11,14 @@ while i != 1:
         l.append(st[j].lower())
       else:
         l.append(st[j].capitalize())
-    print(''.join(l))
+    f = ''.join(l).split()
+    for k in range(len(f)):
+        if(k == 1):
+            if(f[k] == "date"):
+                print("String " + f[0])
+            if(f[k] == "numeric"):
+                print("BigDecimal " + f[0])
+            if(f[k] == "int"):
+                print("int " + f[0])
+            if(f[k] == "bit"):
+                print("int " + f[0])
